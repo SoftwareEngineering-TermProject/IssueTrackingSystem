0878eb4fdd7f654870baf9b8805c59b40e3dc06f
bbakbum <zxc534@naver.com>
2024-05-27 00:08:25 +0900

    implement comment

--- src/main/resources/static/javascript/issue_member.js
+++ src/main/resources/static/javascript/issue_member.js
@@ -16,7 +16,7 @@ window.addEventListener("click", (event)=> {
     }
 });
 
-var project_users;
+var project_users = null;
 //load all project participants
 function loadAllMembers(){
     const userRequest = new XMLHttpRequest();
@@ -43,6 +43,9 @@ function loadAllMembers(){
                 const div = document.createElement("div");
                 div.innerText = element.userName;
                 div.classList.add("element");
+                if(element.userId == localStorage.getItem("userId")){
+                    div.classList.add("self");
+                }
                 if (element.userRole == "PL") {
                     pl_list.appendChild(div);
                 } else if (element.userRole == "DEV") {
@@ -56,7 +59,6 @@ function loadAllMembers(){
                 option.value = element.userName;
                 assignee.appendChild(option);
             })
-            
         } else {
             alert("프로젝트의 유저 목록을 받아오지 못했습니다.");
             console.error("Error", userRequest.status, userRequest.statusText);
@@ -65,3 +67,10 @@ function loadAllMembers(){
 }
 
 loadAllMembers();
+const checkVariable = setInterval(() => {
+    if (project_users !== null) {
+        // 변수가 null이 아니면 함수를 실행하고 주기적인 체크를 멈춥니다.
+        loadAllUsers();
+        clearInterval(checkVariable);
+    }
+}, 100);
